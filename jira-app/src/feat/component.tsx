1548cb9ca2797c23f21b3363ea8c979b6ae18da8
Zaven Jumayan <zavenjumayan@gmail.com>
2025-02-15 21:40:41 +0400

    added components

---
+++ jira-app/src/feat/component.tsx
@@ -0,0 +1,9 @@
+import{Button} from "@/components/ui/button";
+
+export const TestComponent=()=>{
+    return (
+        <div className="bg-green-500">
+            <p>Hello</p>
+        </div>
+    );
+};
